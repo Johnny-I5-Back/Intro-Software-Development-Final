e21cf0657a50cf4e4c09d370e8147f4b16b9acd5
Johnny-I5-Back <94026273+Johnny-I5-Back@users.noreply.github.com>
2022-03-06 20:55:35 -0600

    Update finalGUI.py
    added in comments

--- finalGUI.py
+++ finalGUI.py
@@ -4,18 +4,20 @@ from tkinter import END, PhotoImage, messagebox
 
 class WrxVisualizer(tk.Tk):
 
+    #tkinter settings
     def __init__(self):
         super().__init__()
-        self.title("Subaru Wrx Visualizer")
-        self.geometry("400x600")
-        self.resizable(False,False)
-        self.createWidgets()
+        self.title("Subaru Wrx Visualizer") #window title
+        self.geometry("400x600") #window size
+        self.resizable(False,False) #makes it where you cant change window dimensions
+        self.createWidgets() # calls the create widgets function
 
     def createWidgets(self):
         """creates the widgets for the gui"""
 
         #displays the defualt car model
         def addDefaultImg():
+            """Adds a label for the image and adds in the default model for the program"""
             img_label = tk.Label(self,width=300,height=200)
             img_label.image = PhotoImage(file="images/bluebase.png")
             img_label['image'] = img_label.image
@@ -23,8 +25,9 @@ class WrxVisualizer(tk.Tk):
 
         #handles the users choice of color selection
         def viewSelectedColor():
-            choice = colorVar.get()
-            displayInfo.delete('1.0',END)
+            """displays what color the user has selected"""
+            choice = colorVar.get() #gets the users radio selection choice
+            displayInfo.delete('1.0',END) #deletes any text in the text box
 
             if(choice == 1):
                 displayInfo.insert('1.0', 'You Selected Blue')
@@ -35,8 +38,9 @@ class WrxVisualizer(tk.Tk):
 
         #handles the users choice of spoiler selection
         def addSpoiler():
+            """displays the spoiler choice the user has made"""
             includeSpoiler = spoilerVar.get()
-            displayInfo.delete('1.0',END)
+            displayInfo.delete('1.0',END) #deletes any text currently in the text field
 
             if(includeSpoiler == 1):
                 displayInfo.insert('1.0', 'You Selected A Spoiler')
@@ -48,10 +52,12 @@ class WrxVisualizer(tk.Tk):
                 displayInfo.insert('1.0', 'Select An Option')
 
         def saveProjectName():
+            """returns the name the user enters for the project"""
             return projectName.get()
 
         #when user clicks confirm a pop up appears, if yes a new window is created displaying the final project information
         def confirmCustomizer(colorVar,spoilerVar):
+            """takes the user selection from the radio buttons and returns the final image based off of the results"""
             color = colorVar.get()
             spoiler = spoilerVar.get()
             
@@ -59,9 +65,9 @@ class WrxVisualizer(tk.Tk):
             proceed = messagebox.askyesno('Confirmation Window', 'Do you want to proceed?')
             #if pop up is yes/true create a new window
             if(proceed == True):
-                finalWindow = tk.Toplevel()
-                finalWindow.geometry("400x400")
-                finalWindow.resizable(False,False)
+                finalWindow = tk.Toplevel() #creates new tkinter window 
+                finalWindow.geometry("400x400") #new window size
+                finalWindow.resizable(False,False) #not resizeable
 
                 displayProjectName = tk.Text(finalWindow)
                 displayProjectName.insert('1.0', saveProjectName())
@@ -70,6 +76,8 @@ class WrxVisualizer(tk.Tk):
                 finalinfo = tk.Text(finalWindow)
                 finalinfo.place(x=0,y=20)
 
+                #if color is equal to 1 returns blue if 2 returns black
+                #if spoiler is equal to 1 returns with spoiler if 2 returns without spoiler
                 #displays blue wrx with spoiler
                 if(color == 1 and spoiler == 1):
                     finalinfo.insert('1.0', 'You selected blue with a spoiler.')                    
@@ -110,33 +118,38 @@ class WrxVisualizer(tk.Tk):
                     img_label['image'] = img_label.image
                     img_label.place(x=0,y=40)
 
-
+                #creates and styles exit button
                 exitBtn = tk.Button(finalWindow, text="Exit program",bg="red", fg="white", padx=5,pady=5, command=self.quit)
                 exitBtn.place(x=0,y=250)
 
                 finalWindow.mainloop()
+
+            #if user clicks no on the pop up, the pop up closes and returns to the main gui window
             else:
                 self.quit
 
         
 
-    
+        #ask user to enter name of project
         promptLbl = tk.Label(self,text="Choose a name for the project.")
         promptLbl.place(x=0,y=0)
 
+        #input field for project name
         projectName = tk.Entry(self)
         projectName.place(x=0,y=20)
-
+        
+        #save project name button calls the function that stores the name
         saveProjectBtn = tk.Button(self, text="Continue",command=saveProjectName)
         saveProjectBtn.place(x=0,y=40)
 
-        #add the default image
+        #add the default image called above
         addDefaultImg()
 
         #color prompt
         colorlbl = tk.Label(self, text="Choose the base color: ")
         colorlbl.place(x=0,y=225)
 
+        #creates a radio button for the color choices
         colorVar = tk.IntVar(self)
         blue = tk.Radiobutton(self,text="Blue", variable=colorVar,value=1,command=viewSelectedColor)
         blue.place(x=0,y=250)
@@ -148,19 +161,19 @@ class WrxVisualizer(tk.Tk):
         spoilerSelectLbl = tk.Label(self,text="Add Spoiler: ")
         spoilerSelectLbl.place(x=0,y=300)
 
+        #creates the radio buttons for the spoiler choice
         spoilerVar = tk.IntVar(self)
         withSpoiler = tk.Radiobutton(self,text="With Spoiler", variable=spoilerVar,value=1,command=addSpoiler)
         withSpoiler.place(x=0,y=325)
-        
         withoutSpoiler = tk.Radiobutton(self,text="Without Spoiler", variable=spoilerVar,value=2,command=addSpoiler)
         withoutSpoiler.place(x=0,y=350)
 
-        #selection
+        #displays the information when a user selects an option from the radio buttons
         displayInfo = tk.Text(self,height=2)
         displayInfo.place(x=0,y=400)
         
         
-        #confirmation button
+        #confirmation button calls the function that creates a new window and displays final image
         confirmationBtn = tk.Button(self,text="Confirm Setup", bg="green", fg="white", padx=5, pady=5, command = lambda: confirmCustomizer(colorVar,spoilerVar))
         confirmationBtn.place(x=0,y=450)
 
@@ -168,9 +181,9 @@ class WrxVisualizer(tk.Tk):
         exitBtn = tk.Button(self, text="Exit program", bg="red",fg="white", padx=5,pady=5, command=self.quit)
         exitBtn.place(x=100,y=450)
         
-
 def main():
     WrxVisualizer().mainloop()
 
+#calls main
 if __name__ == "__main__":
     main()
